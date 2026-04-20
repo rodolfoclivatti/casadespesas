850f9c99179f26d5fb7e3f7aaf75135140de2ae6
[dyad] <rodolfoclivatti@gmail.com>
2026-04-20 13:45:59 -0300

    [dyad] Downgrade para React 18.3.1 para resolver conflito de ambiente - wrote 3 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./globals.css";
 
 const container = document.getElementById("root");
 if (container) {
-  const root = createRoot(container);
+  const root = ReactDOM.createRoot(container);
   root.render(
     <React.StrictMode>
       <App />
